78641e024041af535b241d186c9ee17549a3002f
sylvie <sylvie.uxe@gmail.com>
2024-03-17 18:47:01 -0400

    Enabled Tina Search

--- tina/config.ts
+++ tina/config.ts
@@ -98,4 +98,12 @@ export default defineConfig({
       },
     ],
   },
+  search: {
+    tina: {
+      indexerToken: process.env.TINA_SEARCH_TOKEN,
+      stopwordLanguages: ['fra'],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100,
+  },
 });
